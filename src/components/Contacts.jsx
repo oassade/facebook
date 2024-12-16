7790b95a2c3af93dd67cec6a651ca028c5eb18d0
oassade <oassade@gmail.com>
2024-12-16 19:43:51 +0100

    second day

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Contact from './Contact'
+import axios  from "axios";
 
 function Contacts() {
+    const [contacts, setContacts] = useState([]);
+    
+    useEffect(() => {
+        axios.get("/data/contacts.json").then(res => {
+            setContacts(res.data);
+        })
+    }, []);
+
+
+
     return (
         <div className="online">
             <h6 className="title">
                 contact
             </h6>
-            <Contact profilePicture="/img/avatar/1.jpg" name="Jhon Doe" />
-            <Contact profilePicture="/img/avatar/2.jpg" name="Sarah Smith" />
-            <Contact profilePicture="/img/avatar/3.jpg" name="Jane Doe" />
-
+            <button>Change color</button>
+            {
+                contacts && contacts.map((contact, index) => {
+                    return <Contact key={index} profilePicture={contact.profilePicture} name={contact.name} />
+                })
+            } 
         </div>
     )
 }
