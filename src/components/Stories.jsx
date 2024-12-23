e35714add9de938412c3347bf7df419e869b4801
oassade <oassade@gmail.com>
2024-12-23 19:08:53 +0100

    23dec

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
 import Story from './Story'
+import axios from 'axios';
 
+const options = {
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    smartSpeed: 1000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 5
+        }
+    }
+}
 function Stories() {
+    const [stories, setStories] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+      axios.get("/data/stories.json").then(res => {
+        setStories(res.data)
+        setIsLoaded(true)
+      })  
+    }, []);
+
+
     return (
         <div className="stories">
             <div className="owl-controls">
@@ -11,25 +47,28 @@ function Stories() {
                     </div>
                 </div>
             </div>
-            <div className="owl-carousel owl-theme slider">
+            { isLoaded ? (
+                <OwlCarousel className='owl-theme owl-carousel slider' {...options}>
                 <div className="item">
-                    <div className="overlay first">
-                        <div className="create">
-                            <div className="icon">
-                                <img src="img/icons/plus.svg" alt />
+                        <div className="overlay first">
+                            <div className="create">
+                                <div className="icon">
+                                    <img src="img/icons/plus.svg" alt />
+                                </div>
+                                <span>Create a Story</span>
                             </div>
-                            <span>Create a Story</span>
+                        </div>
+                        <div className="story-image">
+                            <img src="img/avatar/hero.png" alt />
                         </div>
                     </div>
-                    <div className="story-image">
-                        <img src="img/avatar/hero.png" alt />
-                    </div>
-                </div>
-                <Story title="Script House" image="img/stories/st-1.jpeg" profilePicture="img/avatar/1.jpg" />
-                <Story title="Storie two" image="img/stories/st-2.jpeg" profilePicture="img/avatar/2.jpg" />
-                <Story title="Storie  three" image="img/stories/st-3.jpeg" profilePicture="img/avatar/3.jpg" />
-                <Story title="Storie  four" image="img/stories/st-4.jpg" profilePicture="img/avatar/4.jpg" />
-            </div>
+                     {stories.map(story => (
+                         <Story title={story.title} image={story.image} profilePicture={story.profilePicture} />
+                     ))}
+                </OwlCarousel>
+            ) : <>No data yet</> }
+            
+           
         </div>
     )
 }
